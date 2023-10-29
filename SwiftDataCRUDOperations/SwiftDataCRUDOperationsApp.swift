088d878e56923eca064b6fa93a3ee728f427e9a0
ram4ik <ram4ik@hotmail.com>
2023-10-29 19:42:56 +0200

    Initial Commit

---
+++ SwiftDataCRUDOperations/SwiftDataCRUDOperationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataCRUDOperationsApp.swift
+//  SwiftDataCRUDOperations
+//
+//  Created by admin on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataCRUDOperationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
