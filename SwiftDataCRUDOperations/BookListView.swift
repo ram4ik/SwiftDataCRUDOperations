2c8027b50a6299b2960d6ea6aa8bcd87e3e911f4
ram4ik <ram4ik@hotmail.com>
2023-10-29 20:35:05 +0200

    onDelete book

--- SwiftDataCRUDOperations/BookListView.swift
+++ SwiftDataCRUDOperations/BookListView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import SwiftData
 
 struct BookListView: View {
+    @Environment(\.modelContext) private var context
     @Query(sort: \Book.title) private var books: [Book]
     @State private var createNewBook = false
     
@@ -43,10 +44,17 @@ struct BookListView: View {
                                 }
                             }
                         }
+                        .onDelete { indexSet in
+                            indexSet.forEach { index in
+                                let book = books[index]
+                                context.delete(book)
+                            }
+                        }
+                        
                     }
+                    .listStyle(.plain)
                 }
             }
-            .listStyle(.plain)
             .padding()
             .navigationTitle("My Books")
             .toolbar {
